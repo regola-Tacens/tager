c4110defb1f4982745c5d035c113ae8dcc51808e
regola-Tacens <regolas_tacens@outlook.fr>
2021-05-22 10:52:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ImageTagger from "./tagger/ImageTagger";
+
+
+function App() {
+  return (
+    <div>
+      
+      <ImageTagger src="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tagger/ImageTagger.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+
+const ImageTagger = ({ src }) => {
+    const [tags, setTag]=useState([])
+
+    const createTag =async (e)=> {
+        let tag = await window.prompt("write your tag")
+        const { clientX, clientY, target } = e
+        // const { x, y, width, height } = target.getBoundingClientRect()git
+        setTag(state => [
+            ...state, {
+                posX: clientX, 
+                posY: clientY,
+                message: tag,
+            }
+        ])
+        
+
+    }
+
+    return ( 
+        <div>
+             <img src={src} alt="qq" onClick={createTag} />
+        </div>
+     );
+}
+ 
+export default ImageTagger;
+
